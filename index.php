590f107533895bbfad77a9c415acf578ae766a5a
SirRahal <biggyistheman@yahoo.com>
2013-10-02 09:38:01 -0400

    added index for test
    testing

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title id='title'>321Done</title>
+        <link rel="shortcut icon" type="image/x-icon" href="images/icons/favicon.png" />
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+    </head>
+</head>
+
+
+<body>
+testing index 2
+</body>
+</html>
